b20373f119310b9d06689dc9494ab65861678a9e
kolorob02 <alviahmed811@gmail.com>
2019-03-13 17:24:12 +0600

    adding admin template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,114 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>Sign In | Donatio</title>
+    <!-- Favicon-->
+<link rel="icon" href="{{ asset('admin/favicon.ico') }}" type="image/x-icon">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <!-- Bootstrap Core Css -->
+<link href="{{ asset('admin/plugins/bootstrap/css/bootstrap.css') }}" rel="stylesheet">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+    <!-- Waves Effect Css -->
+<link href="{{ asset('admin/plugins/node-waves/waves.css') }}" rel="stylesheet" />
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+    <!-- Animation Css -->
+<link href="{{ asset('admin/plugins/animate-css/animate.css') }}" rel="stylesheet" />
+
+    <!-- Custom Css -->
+<link href="{{ asset('admin/css/style.css') }}" rel="stylesheet">
+
+    <style>
+        body{
+            background-image: url({{ asset('admin/images/image-gallery/landscape.jpg') }});
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    </style>
+</head>
+
+<body class="login-page">
+    <div class="login-box">
+        <div class="logo">
+            <a href="javascript:void(0);">Admin<b>BSB</b></a>
+            <small>Admin BootStrap Based - Material Design</small>
+        </div>
+        <div class="card">
+            <div class="body">
+                <form id="sign_in" method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="msg">Sign in to start your session</div>
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="material-icons">person</i>
+                        </span>
+                        <div class="form-line {{ $errors->has('email') ? 'focused error' : ''}}">
+                            <input type="text" class="form-control" name="email" value="{{ old('email') }}" placeholder="Username" required autofocus>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        @if ($errors->has('email'))
+                            <span class="col-pink" role="alert">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="material-icons">lock</i>
+                        </span>
+                        <div class="form-line {{ $errors->has('password') ? 'focused error' : ''}}">
+                            <input type="password" class="form-control" name="password" placeholder="Password" required>
                         </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
+                        @if ($errors->has('password'))
+                            <span class="col-pink" role="alert">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-8 p-t-5">
+                            <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }} class="filled-in chk-col-pink">
+                            <label for="remember">Remember Me</label>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+                        <div class="col-xs-4">
+                            <button class="btn btn-block bg-pink waves-effect" type="submit">SIGN IN</button>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                    <div class="row m-t-15 m-b--20">
+                        <div class="col-xs-6">
+                            <a href="sign-up.html">Register Now!</a>
+                        </div>
+                        @if (Route::has('password.request'))
+                            <div class="col-xs-6 align-right">
+                                <a href="forgot-password.html">Forgot Password?</a>
+                            </div>
+                        @endif
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-</div>
-@endsection
+
+    <!-- Jquery Core Js -->
+    <script src="{{ asset('admin/plugins/jquery/jquery.min.js') }}"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="{{ asset('admin/plugins/bootstrap/js/bootstrap.js') }}"></script>
+
+    <!-- Waves Effect Plugin Js -->
+    <script src="{{ asset('admin/plugins/node-waves/waves.js') }}"></script>
+
+    <!-- Validation Plugin Js -->
+    <script src="{{ asset('admin/plugins/jquery-validation/jquery.validate.js') }}"></script>
+
+    <!-- Custom Js -->
+    <script src="{{ asset('admin/js/admin.js') }}"></script>
+    <script src="{{ asset('admin/js/pages/examples/sign-in.js') }}"></script>
+</body>
+
+</html>
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>@@yield('page_title')</title>
+    <!-- Favicon-->
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+
+    @section('stylesheets')
+        <!-- Bootstrap Core Css -->
+        <link href="{{ asset('admin/plugins/bootstrap/css/bootstrap.css') }}" rel="stylesheet">
+
+        <!-- Waves Effect Css -->
+        <link href="{{ asset('admin/plugins/node-waves/waves.css') }}" rel="stylesheet" />
+
+        <!-- Animation Css -->
+        <link href="{{ asset('admin/plugins/animate-css/animate.css') }}" rel="stylesheet" />
+
+        <!-- Custom Css -->
+        <link href="{{ asset('admin/css/style.css') }}" rel="stylesheet">
+
+        <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
+        <link href="{{ asset('admin/css/themes/all-themes.css') }}" rel="stylesheet" />
+
+        @stack('push_stylesheets')
+    @show
+</head>
+
+<body class="theme-red">
+    <!-- Page Loader -->
+    <div class="page-loader-wrapper">
+        <div class="loader">
+            <div class="preloader">
+                <div class="spinner-layer pl-red">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                </div>
+            </div>
+            <p>Please wait...</p>
+        </div>
+    </div>
+    <!-- #END# Page Loader -->
+    <!-- Overlay For Sidebars -->
+    <div class="overlay"></div>
+    <!-- #END# Overlay For Sidebars -->
+    <!-- Search Bar -->
+    <div class="search-bar">
+        <div class="search-icon">
+            <i class="material-icons">search</i>
+        </div>
+        <input type="text" placeholder="START TYPING...">
+        <div class="close-search">
+            <i class="material-icons">close</i>
+        </div>
+    </div>
+    <!-- #END# Search Bar -->
+    @include('layouts.topbar')
+    <section>
+        @include('layouts.leftbar')
+    </section>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="block-header">
+                <h2>yield('page_title')</h2>
+            </div>
+
+            @section('error_message')
+                @if(count($errors)>0)
+
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>
+                                Error
+                            </h2>
+                        </div>
+                        <div class="body">
+                            <div class="alert alert-danger">
+                                <strong>Oh snap!</strong> Change a few things up and try submitting again.
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{!! $error !!}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endif
+            @show
+
+            @if (session('status'))
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="card">
+                    <div class="body">
+                        <div class="alert">
+                            {{ session('status') }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+             @endif
+
+            @yield('content')
+        </div>
+    </section>
+
+    @section('javascripts')
+        <!-- Jquery Core Js -->
+        <script src="{{ asset('admin/plugins/jquery/jquery.min.js') }}"></script>
+
+        <!-- Bootstrap Core Js -->
+        <script src="{{ asset('admin/plugins/bootstrap/js/bootstrap.js') }}"></script>
+
+        <!-- Select Plugin Js -->
+        <script src="{{ asset('admin/plugins/bootstrap-select/js/bootstrap-select.js') }}"></script>
+
+        <!-- Slimscroll Plugin Js -->
+        <script src="{{ asset('admin/plugins/jquery-slimscroll/jquery.slimscroll.js') }}"></script>
+
+        <!-- Waves Effect Plugin Js -->
+        <script src="{{ asset('admin/plugins/node-waves/waves.js') }}"></script>
+
+        <!-- Jquery CountTo Plugin Js -->
+        <script src="{{ asset('admin/plugins/jquery-countto/jquery.countTo.js') }}"></script>
+
+        <!-- Custom Js -->
+        <script src="{{ asset('admin/js/admin.js') }}"></script>
+        <script src="{{ asset('admin/js/pages/index.js') }}"></script>
+
+        <!-- Demo Js -->
+        <script src="{{ asset('admin/js/demo.js') }}"></script>
+    @show
+    @stack('push_javascript')
+</body>
+
+</html>
