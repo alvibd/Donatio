a99a078b63d1438aa2842c343c472487f4f93015
kolorob02 <alviahmed811@gmail.com>
2019-03-14 16:11:58 +0600

    added user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function getUsers(Request $request)
+    {
+        $user = [];
+        if (Auth::user()->hasRole('superadministrator')) {
+            $user = User::paginate(20);
+        }
+        else {
+            abort();
+        }
+
+        return view();
+    }
+}
