03f57191e4fed40e989ace55d45ce9db149f2149
Alvi Ahmed <alviahmed811@gmail.com>
2019-04-02 12:25:22 +0600

    created query log for all queries

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        DB::listen(function($query){
+            Log::channel('query')->info($query->sql);
+        });
     }
 }
